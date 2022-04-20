76dcb3294f7aa4a8345419137f936556f91cd591
Caleeeb <daycinematography@gmail.com>
2022-04-20 15:42:44 -0400

    validate name prompts

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?'
+                message: 'What is your name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('please enter a valid name');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter your GitHub Username'
+                message: 'Enter your GitHub Username',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('please enter a valid username');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
@@ -55,7 +71,15 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'description',
-                message: 'Provide a description of the project (Required)'
+                message: 'Provide a description of the project (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('please give a description');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'checkbox',
@@ -66,7 +90,15 @@ const promptProject = portfolioData => {
             {
                 type: 'input',
                 name: 'link',
-                message: 'Enter the GitHub link to your project. (Required)'
+                message: 'Enter the GitHub link to your project. (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('please enter a valid link');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'confirm',
